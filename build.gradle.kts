2ca9655980a27ceee155ffa931ba0d84513a0115
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2019-10-21 17:19:07 +0200

    feat(WIP): working authentication setup with Keycloak
    - Redirect to Keycloak if user not logged in
    - Needs some documentation and polishing
    - Needs Keycloak adaptations to be pushed back to datahub-launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.1.7.RELEASE"
+	id("org.springframework.boot") version "2.2.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
@@ -16,12 +16,15 @@ repositories {
 	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
-extra["springCloudVersion"] = "Greenwich.SR3"
+extra["springCloudVersion"] = "Hoxton.M3"
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.cloud:spring-cloud-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
--- src/main/kotlin/com/egm/datahub/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/egm/datahub/apigateway/ApiGatewayApplication.kt
@@ -4,16 +4,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ApiGatewayApplication {
+class ApiGatewayApplication(
+		private val filterFactory: TokenRelayGatewayFilterFactory
+) {
 
 	@Bean
 	fun myRoutes(builder: RouteLocatorBuilder): RouteLocator {
 		return builder.routes()
 				.route { p ->
 					p.path("/ngsi-ld/v1/entities")
+							.filters {
+								it.filter(filterFactory.apply())
+							}
 							// TODO : configurable version
 							.uri("http://context-registry:8081")
 				}

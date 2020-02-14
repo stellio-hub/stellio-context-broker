b60c9dd4a276baa127431c0bdaf5c02a5668f4e3
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2020-02-14 11:08:19 +0100

    fix: proper escaping of parenthesis around observation dates

--- src/main/kotlin/com/egm/datahub/context/search/service/EntityService.kt
+++ src/main/kotlin/com/egm/datahub/context/search/service/EntityService.kt
@@ -81,7 +81,7 @@ class EntityService(
 
             // insert the values property with data retrieved from DB
             val valuesKey = NgsiLdParsingUtils.expandJsonLdKey("values", rawEntity.second)
-            val simplifiedValues = it.map { "[${it["VALUE"]}, \"${it["OBSERVED_AT"]}\"]" }.joinToString(",")
+            val simplifiedValues = it.map { """[${it["VALUE"]}, "${it["OBSERVED_AT"]}"]""" }.joinToString(",")
             propertyToEnrich[valuesKey!!] = listOf(simplifiedValues)
 
             // and finally update the raw entity with the updated temporal property

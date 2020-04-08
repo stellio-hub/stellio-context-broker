bcea1803be7bfe6db77a80b2ae8e1ddcec0ffc88
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2020-04-08 09:21:08 +0000

    Merged in fix/1102-ignore-unknown-subscription-properties (pull request #33)

--- shared/src/main/kotlin/com/egm/stellio/shared/model/Subscription.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/Subscription.kt
@@ -1,5 +1,9 @@
 package com.egm.stellio.shared.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+// it is a lightweight subscription representation, so ignore other fields from a complete subscription
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Subscription(
     val id: String,
     val type: String = "Subscription",

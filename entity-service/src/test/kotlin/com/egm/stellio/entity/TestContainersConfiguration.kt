4259cd7e1c2821f489fa3be0582a7f444601012f
Vincent <vincent.r06@gmail.com>
2020-05-15 09:31:40 +0200

    fix: load TestContainersConfiguration only when needed by a test #22
    - fix: load when needed TestContainersConfiguration
    - fix broken test after removal of Neo4J DB
    - fix: use relaxed Mock bean to avoid boilerplate code
    
    Co-authored-by: Vincent Raybaud <vincent.raybaud@eglobalmark.com>

--- entity-service/src/test/kotlin/com/egm/stellio/entity/TestContainersConfiguration.kt
+++ entity-service/src/test/kotlin/com/egm/stellio/entity/TestContainersConfiguration.kt
@@ -1,11 +1,11 @@
 package com.egm.stellio.entity
 
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.DockerComposeContainer
 import java.io.File
 
-@Configuration
+@TestConfiguration
 class TestContainersConfiguration {
 
     private val DB_USER = "neo4j"

6d5c30a66a934129ddd3f446b0b6be7442404912
houcem.kacem <houcem.kacem@eglobalmark.com>
2020-05-25 16:04:13 +0200

    feat: use of produces element of @RequestMapping to check accept header + some code refactoring #25

--- shared/src/test/kotlin/com/egm/stellio/shared/util/MockkedHandler.kt
+++ shared/src/test/kotlin/com/egm/stellio/shared/util/MockkedHandler.kt
@@ -9,15 +9,6 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/router/mockkedroute")
 class MockkedHandler {
 
-    @GetMapping
-    fun get() = ResponseEntity.status(HttpStatus.OK).build<String>()
-
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE, JSON_LD_CONTENT_TYPE])
     fun post() = ResponseEntity.status(HttpStatus.CREATED).build<String>()
-
-    @PatchMapping(consumes = [MediaType.APPLICATION_JSON_VALUE, JSON_LD_CONTENT_TYPE, JSON_MERGE_PATCH_CONTENT_TYPE])
-    fun patch() = ResponseEntity.status(HttpStatus.NO_CONTENT).build<String>()
-
-    @DeleteMapping
-    fun delete() = ResponseEntity.status(HttpStatus.NO_CONTENT).build<String>()
 }

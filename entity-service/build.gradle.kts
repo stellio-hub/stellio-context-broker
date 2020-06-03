e4c31f5dd89327d21c96f56173942c10615a2ecb
Houcem Kacem <houcem.kacem@eglobalmark.com>
2020-06-03 11:55:42 +0200

    fix(entity): remove liquigraph tool and add neo4j-migrations tool to manage neo4j migrations #53
    Issue number: #53  
    Pull request number: #54

--- entity-service/build.gradle.kts
+++ entity-service/build.gradle.kts
@@ -15,11 +15,9 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.neo4j:neo4j-ogm-bolt-native-types")
-    implementation("org.liquigraph:liquigraph-spring-boot-starter:4.0.1")
     implementation("org.jgrapht:jgrapht-core:1.4.0")
-
+    implementation("eu.michael-simons.neo4j:neo4j-migrations-spring-boot-starter:0.0.11")
     implementation(project(":shared"))
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")

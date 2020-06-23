1b3d30504fc9510614afbc1a269833e88b57bb0b
vraybaud <vincent.raybaud@eglobalmark.com>
2020-06-23 17:05:58 +0200

    fix(search): extend size of stellio_search db columns #84 columns : type, attribute_value_type, attribute_name

---
+++ search-service/src/main/resources/db/migration/V0_9__extend_size_type_attribute_name_attribute_value_type_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE temporal_entity_attribute ALTER COLUMN type TYPE VARCHAR(255);
+ALTER TABLE temporal_entity_attribute ALTER COLUMN attribute_name TYPE VARCHAR(255);
+ALTER TABLE temporal_entity_attribute ALTER COLUMN attribute_value_type TYPE VARCHAR(255);
+

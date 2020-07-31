374ee59edb7f87d71b902c46e85b0c637b8a4e64
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2020-07-31 15:32:55 +0200

    fixup(search): entity payload SQL migration script was creating duplicates

--- search-service/src/main/resources/db/migration/V0_12__add_entity_payload_table.sql
+++ search-service/src/main/resources/db/migration/V0_12__add_entity_payload_table.sql
@@ -6,8 +6,16 @@ CREATE TABLE entity_payload(
 
 -- Move existing entity_payload rows to entity_payload table
 INSERT INTO entity_payload
-SELECT DISTINCT entity_id, entity_payload
+SELECT DISTINCT entity_id
 FROM temporal_entity_attribute;
+
+UPDATE entity_payload
+SET payload = (
+    SELECT entity_payload
+    FROM temporal_entity_attribute
+    WHERE entity_payload.entity_id = temporal_entity_attribute.entity_id
+    LIMIT 1
+);
 
 -- Drop existing entity_payload column in temporal_entity_attribute
 ALTER TABLE temporal_entity_attribute DROP entity_payload;

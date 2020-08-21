7b7b107699ac1924527a0b75eaddfa5009e580d0
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2020-08-21 11:09:20 +0200

    feat: auto create API tests user when api-tests profile is active #139

---
+++ entity-service/src/main/kotlin/com/egm/stellio/entity/util/ApiTestsBootstrapper.kt
@@ -0,0 +1,52 @@
+package com.egm.stellio.entity.util
+
+import com.egm.stellio.entity.model.Entity
+import com.egm.stellio.entity.model.Property
+import com.egm.stellio.entity.repository.EntityRepository
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Profile("api-tests")
+@Component
+class ApiTestsBootstrapper(
+    private val entityRepository: EntityRepository
+) : CommandLineRunner {
+
+    @Value("\${application.api-tests.user-id}")
+    val apiTestUserId: String? = null
+
+    companion object {
+        val AUTHORIZATION_CONTEXTS: List<String> = listOf(
+            "https://raw.githubusercontent.com/easy-global-market/ngsild-api-data-models/master/authorization/jsonld-contexts/authorization.jsonld",
+            "http://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"
+        )
+        const val USER_TYPE = "User"
+        val USER_ROLES = listOf("stellio-creator")
+    }
+
+    override fun run(vararg args: String?) {
+        // well, this should not happen in api-tests profile as we start from a fresh database on each run
+        val apiTestsUser = entityRepository.findById(apiTestUserId!!)
+        if (!apiTestsUser.isPresent) {
+            val entity = Entity(
+                id = apiTestUserId!!,
+                type = listOf(USER_TYPE),
+                contexts = AUTHORIZATION_CONTEXTS,
+                properties = mutableListOf(
+                    Property(
+                        name = "roles",
+                        value = USER_ROLES
+                    ),
+                    Property(
+                        name = "username",
+                        value = "API Tests"
+                    )
+                )
+            )
+
+            entityRepository.save(entity)
+        }
+    }
+}

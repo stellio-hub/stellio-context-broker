a65121e2384ff0e7757d01d1970e53a0342b5e13
pstoltz <philippe.stoltz@eglobalmark.com>
2020-09-14 17:47:35 +0200

    plop

--- subscription-service/src/test/kotlin/com/egm/stellio/subscription/service/SubscriptionServiceTests.kt
+++ subscription-service/src/test/kotlin/com/egm/stellio/subscription/service/SubscriptionServiceTests.kt
@@ -67,7 +67,6 @@ class SubscriptionServiceTests : TimescaleBasedTests() {
     @BeforeAll
     fun bootstrapSubscriptions() {
         every { subscriptionsEventsListener.handleSubscriptionEvent(any()) } just Runs
-
         createSubscription1()
         createSubscription2()
         createSubscription3()

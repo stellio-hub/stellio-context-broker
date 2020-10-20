623db1c8467e757ab91ed8258bf16989a57eeee0
pstoltz <philippe.stoltz@eglobalmark.com>
2020-10-20 10:40:49 +0200

    apply dekt remarks

--- shared/src/main/kotlin/com/egm/stellio/shared/util/ApiResponses.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/util/ApiResponses.kt
@@ -2,4 +2,4 @@ package com.egm.stellio.shared.util
 
 const val ENTITY_NOT_FOUND_MESSAGE = "Entity Not Found"
 
-fun entityNotFoundMessage(entityId : String) = "Entity $entityId was not found"
+fun entityNotFoundMessage(entityId: String) = "Entity $entityId was not found"

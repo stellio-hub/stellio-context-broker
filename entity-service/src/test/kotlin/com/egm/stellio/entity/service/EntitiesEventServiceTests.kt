2f77dac98ed3808b2f3d0d60280801596d74ac7c
Houcem Kacem <houcem.kacem@eglobalmark.com>
2020-10-22 10:11:44 +0200

    refactor: refactor code #225

--- entity-service/src/test/kotlin/com/egm/stellio/entity/service/EntitiesEventServiceTests.kt
+++ entity-service/src/test/kotlin/com/egm/stellio/entity/service/EntitiesEventServiceTests.kt
@@ -55,6 +55,8 @@ class EntitiesEventServiceTests {
 
         verify { resolver.resolveDestination("cim.entity.Vehicle") }
 
-        Assertions.assertTrue(message.captured.payload.matchContent(loadSampleData("events/entityCreateEvent.jsonld")))
+        Assertions.assertTrue(message.captured.payload.matchContent(
+            loadSampleData("events/entityCreateEvent.jsonld")
+        ))
     }
 }

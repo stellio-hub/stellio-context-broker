fc1e856db50a4f374eb9ff31dc4fb8242c82d1b8
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2020-12-28 15:13:53 +0100

    fix(subscription): endoint.accept defaults to JSON if not provided

--- subscription-service/src/main/kotlin/com/egm/stellio/subscription/model/Endpoint.kt
+++ subscription-service/src/main/kotlin/com/egm/stellio/subscription/model/Endpoint.kt
@@ -1,11 +1,12 @@
 package com.egm.stellio.subscription.model
 
+import com.egm.stellio.subscription.model.Endpoint.AcceptType.JSON
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.net.URI
 
 data class Endpoint(
     val uri: URI,
-    val accept: AcceptType,
+    val accept: AcceptType = JSON,
     val info: List<EndpointInfo>? = null
 ) {
     fun getInfoValue(key: String): String? {

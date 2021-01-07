276eb01dcd8a3af4f337d53ce74b72d3e2798057
Houcem Kacem <houcem.kacem@eglobalmark.com>
2021-01-07 13:47:51 +0100

    feat(common): allow CORS configuration #289

--- subscription-service/src/main/kotlin/com/egm/stellio/subscription/config/WebConfig.kt
+++ subscription-service/src/main/kotlin/com/egm/stellio/subscription/config/WebConfig.kt
@@ -1,8 +1,7 @@
-package com.egm.stellio.entity.config
+package com.egm.stellio.subscription.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
@@ -13,10 +12,6 @@ class WebConfig : WebFluxConfigurer {
 
     @Value("\${application.cors.allowed_origins}")
     val allowedOrigins: List<String>? = null
-
-    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
-        configurer.defaultCodecs().enableLoggingRequestDetails(true)
-    }
 
     override fun addCorsMappings(registry: CorsRegistry) {
         allowedOrigins?.let { registry.addMapping("/ngsi-ld/v1/**").allowedOrigins(*it.toTypedArray()) }

436553bf9e74864a8a05925cf790f23da78afbbb
Houcem Kacem <houcem.kacem@eglobalmark.com>
2021-01-08 14:52:40 +0100

    feat(common): log allowed origins if defined #289

--- search-service/src/main/kotlin/com/egm/stellio/search/config/WebConfig.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/config/WebConfig.kt
@@ -1,5 +1,6 @@
 package com.egm.stellio.search.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.ServerCodecConfigurer
@@ -14,11 +15,16 @@ class WebConfig : WebFluxConfigurer {
     @Value("\${application.cors.allowed_origins}")
     val allowedOrigins: List<String>? = null
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         configurer.defaultCodecs().enableLoggingRequestDetails(true)
     }
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        allowedOrigins?.let { registry.addMapping("/ngsi-ld/v1/**").allowedOrigins(*it.toTypedArray()) }
+        allowedOrigins?.let {
+            logger.info("Enabling Cross Origin Requests for origins: $it")
+            registry.addMapping("/ngsi-ld/v1/**").allowedOrigins(*it.toTypedArray())
+        }
     }
 }
--- subscription-service/src/main/kotlin/com/egm/stellio/subscription/config/WebConfig.kt
+++ subscription-service/src/main/kotlin/com/egm/stellio/subscription/config/WebConfig.kt
@@ -1,5 +1,6 @@
 package com.egm.stellio.subscription.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
@@ -13,7 +14,12 @@ class WebConfig : WebFluxConfigurer {
     @Value("\${application.cors.allowed_origins}")
     val allowedOrigins: List<String>? = null
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun addCorsMappings(registry: CorsRegistry) {
-        allowedOrigins?.let { registry.addMapping("/ngsi-ld/v1/**").allowedOrigins(*it.toTypedArray()) }
+        allowedOrigins?.let {
+            logger.info("Enabling Cross Origin Requests for origins: $it")
+            registry.addMapping("/ngsi-ld/v1/**").allowedOrigins(*it.toTypedArray())
+        }
     }
 }

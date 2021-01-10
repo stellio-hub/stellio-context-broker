c962a08cef9c5687aeb2da91e1696424c4e9b70e
Houcem Kacem <houcem.kacem@eglobalmark.com>
2021-01-10 17:07:22 +0100

    fix(common): use CorsFilter of spring security for cors configuration #289

--- search-service/src/main/kotlin/com/egm/stellio/search/config/WebSecurityConfig.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/config/WebSecurityConfig.kt
@@ -1,13 +1,21 @@
 package com.egm.stellio.search.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 class WebSecurityConfig {
+
+    @Value("\${application.cors.allowed_origins}")
+    val allowedOrigins: List<String>? = null
+    val allowedMethods = listOf("GET", "POST", "PATCH", "DELETE")
 
     @Bean
     @ConditionalOnProperty("application.authentication.enabled")
@@ -34,5 +42,16 @@ class WebSecurityConfig {
             .authorizeExchange().pathMatchers("/**").permitAll()
 
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.allowedOrigins = allowedOrigins
+        corsConfig.allowedMethods = allowedMethods
+        corsConfig.addAllowedHeader("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/ngsi-ld/v1/**", corsConfig)
+        return source
     }
 }

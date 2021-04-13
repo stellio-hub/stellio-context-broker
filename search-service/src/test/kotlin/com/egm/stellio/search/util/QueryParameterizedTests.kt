b8626bf2e01810990de710e11aa10e83aa47bb89
Benoit Orihuela <benoit.orihuela@eglobalmark.com>
2021-04-13 13:23:54 +0200

    feat: refactor and add tests for the retrieval of the temporal evolution of a relationship

--- search-service/src/test/kotlin/com/egm/stellio/search/util/QueryParameterizedTests.kt
+++ search-service/src/test/kotlin/com/egm/stellio/search/util/QueryParameterizedTests.kt
@@ -3,6 +3,7 @@ package com.egm.stellio.search.util
 import com.egm.stellio.search.model.FullAttributeInstanceResult
 import com.egm.stellio.search.model.SimplifiedAttributeInstanceResult
 import com.egm.stellio.search.model.TemporalEntityAttribute
+import com.egm.stellio.shared.util.loadSampleData
 import com.egm.stellio.shared.util.toUri
 import org.junit.jupiter.params.provider.Arguments
 import java.time.ZonedDateTime
@@ -92,18 +93,83 @@ class QueryParameterizedTests {
                 )
             )
 
+        private val simplifiedResultOfTwoEntitiesWithOnePropertyAndOneRelationship =
+            listOf(
+                Pair(
+                    "urn:ngsi-ld:BeeHive:TESTC".toUri(),
+                    mapOf(
+                        TemporalEntityAttribute(
+                            entityId = "urn:ngsi-ld:BeeHive:TESTC".toUri(),
+                            type = "https://ontology.eglobalmark.com/apic#BeeHive",
+                            attributeName = "https://ontology.eglobalmark.com/apic#incoming",
+                            attributeValueType = TemporalEntityAttribute.AttributeValueType.MEASURE
+                        ) to listOf(
+                            SimplifiedAttributeInstanceResult(
+                                value = 20,
+                                observedAt = ZonedDateTime.parse("2020-03-25T08:33:17.965206Z")
+                            )
+                        ),
+                        TemporalEntityAttribute(
+                            entityId = "urn:ngsi-ld:BeeHive:TESTC".toUri(),
+                            type = "https://ontology.eglobalmark.com/apic#BeeHive",
+                            attributeName = "https://ontology.eglobalmark.com/egm#managedBy",
+                            attributeValueType = TemporalEntityAttribute.AttributeValueType.MEASURE,
+                            attributeType = TemporalEntityAttribute.AttributeType.Relationship
+                        ) to listOf(
+                            SimplifiedAttributeInstanceResult(
+                                value = "urn:ngsi-ld:Beekeeper:1234",
+                                observedAt = ZonedDateTime.parse("2020-03-25T08:33:17.965206Z")
+                            )
+                        )
+                    )
+                ),
+                Pair(
+                    "urn:ngsi-ld:BeeHive:TESTD".toUri(),
+                    mapOf(
+                        TemporalEntityAttribute(
+                            entityId = "urn:ngsi-ld:BeeHive:TESTD".toUri(),
+                            type = "https://ontology.eglobalmark.com/apic#BeeHive",
+                            attributeName = "https://ontology.eglobalmark.com/apic#outgoing",
+                            attributeValueType = TemporalEntityAttribute.AttributeValueType.MEASURE
+                        ) to listOf(
+                            SimplifiedAttributeInstanceResult(
+                                value = 25,
+                                observedAt = ZonedDateTime.parse("2020-03-25T08:33:17.965206Z")
+                            )
+                        ),
+                        TemporalEntityAttribute(
+                            entityId = "urn:ngsi-ld:BeeHive:TESTD".toUri(),
+                            type = "https://ontology.eglobalmark.com/apic#BeeHive",
+                            attributeName = "https://ontology.eglobalmark.com/egm#managedBy",
+                            attributeValueType = TemporalEntityAttribute.AttributeValueType.MEASURE,
+                            attributeType = TemporalEntityAttribute.AttributeType.Relationship
+                        ) to listOf(
+                            SimplifiedAttributeInstanceResult(
+                                value = "urn:ngsi-ld:Beekeeper:5678",
+                                observedAt = ZonedDateTime.parse("2020-03-25T08:33:17.965206Z")
+                            )
+                        )
+                    )
+                )
+            )
+
         @JvmStatic
         fun rawResultsProvider(): Stream<Arguments> {
             return Stream.of(
                 Arguments.arguments(
                     resultOfTwoEntitiesWithOneProperty,
                     false,
-                    "expectations/query/two_beehives.json"
+                    loadSampleData("expectations/query/two_beehives.json")
                 ),
                 Arguments.arguments(
                     simplifiedResultOfTwoEntitiesWithOneProperty,
                     true,
-                    "expectations/query/two_beehives_temporal_values.json"
+                    loadSampleData("expectations/query/two_beehives_temporal_values.json")
+                ),
+                Arguments.arguments(
+                    simplifiedResultOfTwoEntitiesWithOnePropertyAndOneRelationship,
+                    true,
+                    loadSampleData("expectations/query/two_entities_temporal_values_property_and_relationship.json")
                 )
             )
         }

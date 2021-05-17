d3d46dceceb86ce931d7f316ad423288dbc21bd6
Benoit Orihuela <benoit.orihuela@egm.io>
2021-05-17 10:16:27 +0200

    chore(common): upgrade to last maintenance release for Spring Boot and Spring Cloud (#405)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 val detektConfigFile = file("$rootDir/config/detekt/detekt.yml")
 
-extra["springCloudVersion"] = "Hoxton.SR8"
+extra["springCloudVersion"] = "Hoxton.SR11"
 extra["testcontainersVersion"] = "1.15.1"
 
 plugins {
     java // why did I have to add that ?!
     // only apply the plugin in the subprojects requiring it because it expects a Spring Boot app
     // and the shared lib is obviously not one
-    id("org.springframework.boot") version "2.3.4.RELEASE" apply false
+    id("org.springframework.boot") version "2.3.10.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.3.72" apply false
     kotlin("plugin.spring") version "1.3.72" apply false

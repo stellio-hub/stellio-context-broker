c9955439453739b9d418091acd43387ebb0930d8
Houcem Kacem <houcem.kacem@eglobalmark.com>
2021-05-25 14:28:34 +0200

    fix: enable properties configuration

--- entity-service/src/main/kotlin/com/egm/stellio/entity/EntityServiceApplication.kt
+++ entity-service/src/main/kotlin/com/egm/stellio/entity/EntityServiceApplication.kt
@@ -1,10 +1,13 @@
 package com.egm.stellio.entity
 
+import com.egm.stellio.entity.config.ApplicationProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication(scanBasePackages = ["com.egm.stellio.entity", "com.egm.stellio.shared"])
+@EnableConfigurationProperties(ApplicationProperties::class)
 @EnableAsync
 class EntityServiceApplication
 

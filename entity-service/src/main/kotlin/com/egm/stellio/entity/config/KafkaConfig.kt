6fbd8922bcd15078003a107ab0df5ec17ad56da2
Benoit Orihuela <benoit.orihuela@egm.io>
2021-06-18 11:13:25 +0200

    feat(entity): switch to concurrent Kafka container factory (#434)

---
+++ entity-service/src/main/kotlin/com/egm/stellio/entity/config/KafkaConfig.kt
@@ -0,0 +1,23 @@
+package com.egm.stellio.entity.config
+
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler
+
+@Configuration
+class KafkaConfig {
+
+    @Bean
+    fun kafkaListenerContainerFactory(
+        configurer: ConcurrentKafkaListenerContainerFactoryConfigurer,
+        kafkaConsumerFactory: ConsumerFactory<Any?, Any?>?
+    ): ConcurrentKafkaListenerContainerFactory<*, *>? {
+        val factory = ConcurrentKafkaListenerContainerFactory<Any, Any>()
+        configurer.configure(factory, kafkaConsumerFactory)
+        factory.setErrorHandler(SeekToCurrentErrorHandler())
+        return factory
+    }
+}

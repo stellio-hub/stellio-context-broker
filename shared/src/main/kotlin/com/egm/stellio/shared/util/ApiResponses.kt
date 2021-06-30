c577b2cc185a765c55aee30917321bc5784c61d9
Benoit Orihuela <benoit.orihuela@egm.io>
2021-06-30 13:21:57 +0200

    fix detekt error

--- shared/src/main/kotlin/com/egm/stellio/shared/util/ApiResponses.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/util/ApiResponses.kt
@@ -3,5 +3,5 @@ package com.egm.stellio.shared.util
 fun entityNotFoundMessage(entityId: String) = "Entity $entityId was not found"
 fun entityOrAttrsNotFoundMessage(
     entityId: String,
-    attrs: Set<String>) =
-    "Entity $entityId does not exist or it has none of the requested attributes : $attrs"
+    attrs: Set<String>
+) = "Entity $entityId does not exist or it has none of the requested attributes : $attrs"

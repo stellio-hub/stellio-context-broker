404226b8e218768ce08978ab5b7972de1de23c04
Benoit Orihuela <benoit.orihuela@egm.io>
2022-01-03 14:44:56 +0100

    feat(common): activate reuse flag for testcontainers driven containers (#554)
    - remove use of @Container annotation since it automatically stops containers after test
    - must be manually configured locally to take effect: https://github.com/testcontainers/testcontainers-java/pull/1781

--- shared/src/testFixtures/kotlin/com/egm/stellio/shared/support/WithKafkaContainer.kt
+++ shared/src/testFixtures/kotlin/com/egm/stellio/shared/support/WithKafkaContainer.kt
@@ -3,18 +3,17 @@ package com.egm.stellio.shared.support
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.KafkaContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
-@Testcontainers
 interface WithKafkaContainer {
+
     companion object {
 
         private val kafkaImage: DockerImageName = DockerImageName.parse("confluentinc/cp-kafka:5.4.1")
 
-        @Container
-        val kafkaContainer = KafkaContainer(kafkaImage)
+        private val kafkaContainer = KafkaContainer(kafkaImage).apply {
+            withReuse(true)
+        }
 
         @JvmStatic
         @DynamicPropertySource

f7134bfc8f984ac18768428d6d9a7a762a35d785
Benoit Orihuela <benoit.orihuela@egm.io>
2022-04-04 09:33:34 +0200

    fix(subscription): re-add encode url util function

--- shared/src/main/kotlin/com/egm/stellio/shared/util/HttpUtils.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/util/HttpUtils.kt
@@ -1,6 +1,11 @@
 package com.egm.stellio.shared.util
 
 import java.net.URLDecoder
+import java.net.URLEncoder
 
 fun String.decode(): String =
     URLDecoder.decode(this, "UTF-8")
+
+
+fun String.encode(): String =
+    URLEncoder.encode(this, "UTF-8")

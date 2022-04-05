e31e070a0125c496f1486b5214403bbd7856df0d
Benoit Orihuela <benoit.orihuela@egm.io>
2022-04-05 08:40:51 +0200

    fix(subscription): ensure subscription id is a valid URI (#617)

--- subscription-service/src/test/kotlin/com/egm/stellio/subscription/utils/ParsingUtilsTests.kt
+++ subscription-service/src/test/kotlin/com/egm/stellio/subscription/utils/ParsingUtilsTests.kt
@@ -1,8 +1,13 @@
 package com.egm.stellio.subscription.utils
 
+import com.egm.stellio.shared.model.BadRequestDataException
+import com.egm.stellio.shared.util.BEEHIVE_TYPE
 import com.egm.stellio.subscription.model.EndpointInfo
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
@@ -24,5 +29,45 @@ class ParsingUtilsTests {
         val info = ParsingUtils.parseEndpointInfo(input)
 
         assertEquals(info, null)
+    }
+
+    @Test
+    fun `it should not allow a subscription with an empty id`() {
+        val subscription = mapOf(
+            "id" to "",
+            "type" to "Subscription",
+            "entities" to listOf(mapOf("type" to BEEHIVE_TYPE)),
+            "notification" to mapOf("endpoint" to mapOf("uri" to "http://my.endpoint/notifiy"))
+        )
+
+        runBlocking {
+            val result = ParsingUtils.parseSubscription(subscription, emptyList())
+            result.fold({
+                assertTrue(it is BadRequestDataException)
+                assertEquals("The supplied identifier was expected to be an URI but it is not: ", it.message)
+            }, {
+                fail("it should not have allowed a subscription with an empty id")
+            })
+        }
+    }
+
+    @Test
+    fun `it should not allow a subscription with an invalid id`() {
+        val subscription = mapOf(
+            "id" to "invalidId",
+            "type" to "Subscription",
+            "entities" to listOf(mapOf("type" to BEEHIVE_TYPE)),
+            "notification" to mapOf("endpoint" to mapOf("uri" to "http://my.endpoint/notifiy"))
+        )
+
+        runBlocking {
+            val result = ParsingUtils.parseSubscription(subscription, emptyList())
+            result.fold({
+                assertTrue(it is BadRequestDataException)
+                assertEquals("The supplied identifier was expected to be an URI but it is not: invalidId", it.message)
+            }, {
+                fail("it should not have allowed a subscription with an invalid id")
+            })
+        }
     }
 }

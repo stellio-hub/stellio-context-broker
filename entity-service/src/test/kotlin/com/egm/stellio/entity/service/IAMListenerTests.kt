6731c28211529c6ed1c1abe06aaf6d516135a4ce
gpoujol <gael.poujol@egm.io>
2022-04-13 15:30:16 +0200

    feat: modify some test on kafka event

--- entity-service/src/test/kotlin/com/egm/stellio/entity/service/IAMListenerTests.kt
+++ entity-service/src/test/kotlin/com/egm/stellio/entity/service/IAMListenerTests.kt
@@ -42,10 +42,31 @@ class IAMListenerTests {
             entityService.createEntity(
                 match {
                     it.id == "urn:ngsi-ld:User:6ad19fe0-fc11-4024-85f2-931c6fa6f7e0".toUri() &&
-                        it.properties.size == 1 &&
-                        it.properties[0].compactName == "username" &&
-                        it.properties[0].instances.size == 1 &&
-                        it.properties[0].instances[0].value == "stellio"
+                        it.properties.size == 3 &&
+                        it.properties[2].compactName == "username" &&
+                        it.properties[2].instances.size == 1 &&
+                        it.properties[2].instances[0].value == "stellio"
+                }
+            )
+        }
+        confirmVerified()
+    }
+
+    @Test
+    fun `it should parse and transmit user update event`() {
+        val userUpdateEvent = loadSampleData("events/authorization/UserUpdateEvent.json")
+
+        iamListener.processMessage(userUpdateEvent)
+
+        verify {
+            entityService.updateEntityAttributes(
+                "urn:ngsi-ld:User:6ad19fe0-fc11-4024-85f2-931c6fa6f7e0".toUri(),
+                match {
+                    it.size == 1 &&
+                        it[0].compactName == "givenName" &&
+                        it[0] is NgsiLdProperty &&
+                        (it[0] as NgsiLdProperty).instances.size == 1 &&
+                        (it[0] as NgsiLdProperty).instances[0].value == "Jonathan"
                 }
             )
         }

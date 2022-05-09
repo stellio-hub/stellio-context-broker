3bd81dbe908e8accc1857697936c7344098b6af1
Benoit Orihuela <benoit.orihuela@egm.io>
2022-05-09 13:26:42 +0200

    fix(entity): handle previous duplicated entries for an entity

--- search-service/src/main/resources/db/migration/V0_25__swap_client_and_service_account_info.sql
+++ search-service/src/main/resources/db/migration/V0_25__swap_client_and_service_account_info.sql
@@ -1,7 +1,20 @@
+-- since newly created entities have been manually synchronized with the sync endpoint
+-- there are duplicated entities (one row with client id, another one with service account id)
+-- thus first remove duplicates under the service account id key
+delete from entity_access_rights as A
+where subject_id in (select service_account_id from subject_referential)
+and exists (
+    select subject_id
+    from entity_access_rights
+    where subject_id in (select subject_id from subject_referential where subject_type = 'CLIENT')
+    and A.entity_id = entity_access_rights.entity_id
+);
+
+-- then update rows referencing client id to a reference to a service account id
 update entity_access_rights
 set subject_id = (
     select service_account_id
     from subject_referential
     where subject_referential.subject_id = entity_access_rights.subject_id
 )
-where subject_id in (select subject_id from subject_referential where subject_type = 'CLIENT')
+where subject_id in (select subject_id from subject_referential where subject_type = 'CLIENT');

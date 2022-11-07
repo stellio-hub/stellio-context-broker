25474aba1a9e61abfd9174cdefbe21349e6ed1c3
gael poujol <gael.poujol@egm.io>
2022-11-07 18:21:02 +0100

    fix: not remove createdAt and modifiedAt in entities payload

--- search-service/src/main/kotlin/db/migration/V0_28__JsonLd_migration.kt
+++ search-service/src/main/kotlin/db/migration/V0_28__JsonLd_migration.kt
@@ -65,7 +65,6 @@ class V0_28__JsonLd_migration : BaseJavaMigration() {
                 }
 
             // extract specific access policy (if any) from the payload to be able to store it in entity_payload
-            // do the same things for creation and modication dates
             // then remove it from the expanded payload
             val specificAccessPolicy =
                 getAttributeFromExpandedAttributes(originalExpandedEntity, AUTH_PROP_SAP, null)?.let {
@@ -73,18 +72,10 @@ class V0_28__JsonLd_migration : BaseJavaMigration() {
                 }?.let {
                     AuthContextModel.SpecificAccessPolicy.valueOf(it)
                 }
-            // in current implementation, geoproperties do not have a creation date as they are stored
-            // as a property of the entity node, so we give them the creation date of the entity
-            val defaultCreatedAt = getPropertyValueFromMapAsDateTime(
-                originalExpandedEntity as Map<String, List<Any>>,
-                NGSILD_CREATED_AT_PROPERTY
-            )!!
             val expandedEntity = originalExpandedEntity
                 .filterKeys { attributeName ->
                     // remove specific access policy attribute as it is not a "normal" attribute
                     attributeName != AUTH_PROP_SAP
-                    attributeName != NGSILD_CREATED_AT_PROPERTY
-                    attributeName != NGSILD_MODIFIED_AT_PROPERTY
                 }
 
             // store the expanded entity payload instead of the compacted one
@@ -101,6 +92,13 @@ class V0_28__JsonLd_migration : BaseJavaMigration() {
             val jsonLdEntity = JsonLdEntity(expandedEntity, contexts)
             val ngsiLdEntity = jsonLdEntity.toNgsiLdEntity()
 
+            // in current implementation, geoproperties do not have a creation date as they are stored
+            // as a property of the entity node, so we give them the creation date of the entity
+            val defaultCreatedAt = getPropertyValueFromMapAsDateTime(
+                originalExpandedEntity as Map<String, List<Any>>,
+                NGSILD_CREATED_AT_PROPERTY
+            )!!
+
             ngsiLdEntity.attributes.forEach { ngsiLdAttribute ->
                 val attributeName = ngsiLdAttribute.name
                 ngsiLdAttribute.getAttributeInstances().forEach { ngsiLdAttributeInstance ->
@@ -113,6 +111,7 @@ class V0_28__JsonLd_migration : BaseJavaMigration() {
 
                     val attributePayloadFiltered = attributePayload
                         .filterKeys { attributeName ->
+                            //remove createdAt and modifiedAt in attribute's payload
                             attributeName != NGSILD_CREATED_AT_PROPERTY
                             attributeName != NGSILD_MODIFIED_AT_PROPERTY
                         }

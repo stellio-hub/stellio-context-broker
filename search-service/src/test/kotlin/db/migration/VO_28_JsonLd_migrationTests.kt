fd15b9752d355dd9f091216401cf85582590ecd9
gael poujol <gael.poujol@egm.io>
2022-11-23 18:05:49 +0100

    fix: reduce payload size

--- search-service/src/test/kotlin/db/migration/VO_28_JsonLd_migrationTests.kt
+++ search-service/src/test/kotlin/db/migration/VO_28_JsonLd_migrationTests.kt
@@ -11,10 +11,19 @@ class VO_28_JsonLd_migrationTests {
 
     @Test
     fun `it should remove instances when attribute have more than one instance with the same datasetId`() {
-        val payload =
-            loadSampleData("entities_with_two_instances_who_have_same_dataset_id.jsonld").deserializeAsMap()
-        val cleanPayload = payload.keepOnlyOneInstanceByDatsetId()
+        val payload = loadSampleData("entities_with_two_instances_who_have_same_dataset_id.jsonld")
+            .deserializeAsMap()
+            .keepOnlyOneInstanceByDatsetId()
 
-        assertNotEquals(payload, cleanPayload)
+        val expectedPayload =
+            mapOf(
+                "cloudCoverage" to listOf(
+                    mapOf("createdAt" to "2022-07-01T07: 28: 21.450768829Z"),
+                    mapOf("datasetId" to "urn:ngsi-ld:Dataset:Total:Mean"),
+                    mapOf("observedAt" to "2022-07-09T00: 00: 00Z")
+                )
+            )
+
+        assertNotEquals(expectedPayload, payload)
     }
 }

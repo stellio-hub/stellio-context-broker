88e0a6c60d901ac5a1e6a787b0480c300b0dcd82
Benoit Orihuela <benoit.orihuela@egm.io>
2022-12-12 09:23:54 +0100

    feat(subscription): do not serialise default values

--- subscription-service/src/main/kotlin/com/egm/stellio/subscription/model/NotificationParams.kt
+++ subscription-service/src/main/kotlin/com/egm/stellio/subscription/model/NotificationParams.kt
@@ -1,13 +1,16 @@
 package com.egm.stellio.subscription.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.ZonedDateTime
 
 data class NotificationParams(
+    @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
     var attributes: List<String>?,
     val format: FormatType = FormatType.NORMALIZED,
     val endpoint: Endpoint,
     var status: StatusType?,
+    @JsonInclude(value = JsonInclude.Include.NON_DEFAULT)
     val timesSent: Int = 0,
     val lastNotification: ZonedDateTime?,
     val lastFailure: ZonedDateTime?,

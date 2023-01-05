2f586d575abea358e3b2d35eb569a189825758f6
gpoujol <gael.poujol@egm.io>
2023-01-05 12:09:52 +0100

    feat: update bootstrap file for auth test suite

--- search-service/src/main/kotlin/com/egm/stellio/search/support/ApiTestsBootstrapper.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/support/ApiTestsBootstrapper.kt
@@ -16,23 +16,94 @@ class ApiTestsBootstrapper(
     private val subjectReferentialService: SubjectReferentialService
 ) : CommandLineRunner {
 
-    @Value("\${application.apitests.userid}")
-    val apiTestUserId: String? = null
+    @Value("\${application.apitests.userid.1}")
+    val apiTestUserId1: String? = null
+
+    @Value("\${application.apitests.userid.2}")
+    val apiTestUserId2: String? = null
+
+    @Value("\${application.apitests.userid.3}")
+    val apiTestUserId3: String? = null
+
+    @Value("\${application.apitests.groupid.1}")
+    val apiTestGroupId1: String? = null
+
+    @Value("\${application.apitests.groupid.2}")
+    val apiTestGroupId2: String? = null
 
     override fun run(vararg args: String?) {
         runBlocking {
-            subjectReferentialService.retrieve(apiTestUserId!!)
-                .tapLeft {
-                    val subjectReferential = SubjectReferential(
-                        subjectId = apiTestUserId!!,
-                        subjectType = SubjectType.USER,
-                        subjectInfo = """
-                            {"type":"Property","value":{"username":"api-tests-user@stellio.io"}}
-                        """.trimIndent(),
-                        globalRoles = listOf(GlobalRole.STELLIO_CREATOR)
-                    )
-                    subjectReferentialService.create(subjectReferential)
-                }
+            addSubjectToDb(
+                apiTestUserId1!!,
+                userSubject(subjectId = apiTestUserId1!!, groupMembership = apiTestGroupId1)
+            )
+
+            if (!apiTestUserId2.isNullOrEmpty()) {
+                addSubjectToDb(
+                    apiTestUserId2!!,
+                    userSubject(subjectId = apiTestUserId2!!)
+                )
+            }
+
+            if (!apiTestUserId3.isNullOrEmpty()) {
+                addSubjectToDb(
+                    apiTestUserId3!!,
+                    userSubject(subjectId = apiTestUserId3!!, stellioAdmin = true)
+                )
+            }
+
+            if (!apiTestGroupId1.isNullOrEmpty()) {
+                addSubjectToDb(
+                    apiTestGroupId1!!,
+                    groupSubject(apiTestGroupId1!!)
+                )
+            }
+
+            if (!apiTestUserId2.isNullOrEmpty()) {
+                addSubjectToDb(
+                    apiTestGroupId2!!,
+                    groupSubject(apiTestGroupId2!!)
+                )
+            }
         }
+    }
+
+    fun userSubject(
+        subjectId: String,
+        stellioAdmin: Boolean = false,
+        groupMembership: String? = null
+    ): SubjectReferential {
+        return SubjectReferential(
+            subjectId = subjectId!!,
+            subjectType = SubjectType.USER,
+            subjectInfo = """
+                            {"type":"Property","value":{"username":"api-tests-user@stellio.io"}}
+            """.trimIndent(),
+            globalRoles =
+            if (stellioAdmin) listOf(GlobalRole.STELLIO_CREATOR, GlobalRole.STELLIO_ADMIN)
+            else listOf(GlobalRole.STELLIO_CREATOR),
+            groupsMemberships =
+            if (!groupMembership.isNullOrEmpty())
+                listOf(groupMembership!!)
+            else null
+        )
+    }
+
+    fun groupSubject(subjectId: String): SubjectReferential {
+        return SubjectReferential(
+            subjectId = subjectId!!,
+            subjectType = SubjectType.GROUP,
+            subjectInfo = """
+                            {"type":"Property","value":{"username":"api-tests-user@stellio.io"}}
+            """.trimIndent()
+        )
+    }
+
+    suspend fun addSubjectToDb(subjectId: String, subjectReferential: SubjectReferential) {
+        subjectReferentialService.retrieve(subjectId!!)
+            .tapLeft {
+                val subjectReferential = subjectReferential
+                subjectReferentialService.create(subjectReferential)
+            }
     }
 }

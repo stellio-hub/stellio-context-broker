a1d1ef5a995ac165e4c3141330df9cdbb5bbf7d7
Gaël Poujol <76944966+gpoujol@users.noreply.github.com>
2023-01-09 11:20:04 +0100

    feat: update bootstrap file for auth test suite (#823)

--- search-service/src/main/kotlin/com/egm/stellio/search/support/ApiTestsBootstrapper.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/support/ApiTestsBootstrapper.kt
@@ -16,23 +16,88 @@ class ApiTestsBootstrapper(
     private val subjectReferentialService: SubjectReferentialService
 ) : CommandLineRunner {
 
-    @Value("\${application.apitests.userid}")
-    val apiTestUserId: String? = null
+    @Value("\${application.apitests.userid.1}")
+    val apiTestUserId1: String? = null
+
+    @Value("\${application.apitests.userid.2}")
+    val apiTestUserId2: String? = null
+
+    @Value("\${application.apitests.userid.3}")
+    val apiTestUserId3: String? = null
+
+    @Value("\${application.apitests.groupid.1}")
+    val apiTestGroupId1: String? = null
+
+    @Value("\${application.apitests.groupid.2}")
+    val apiTestGroupId2: String? = null
 
     override fun run(vararg args: String?) {
         runBlocking {
-            subjectReferentialService.retrieve(apiTestUserId!!)
-                .tapLeft {
-                    val subjectReferential = SubjectReferential(
-                        subjectId = apiTestUserId!!,
-                        subjectType = SubjectType.USER,
-                        subjectInfo = """
-                            {"type":"Property","value":{"username":"api-tests-user@stellio.io"}}
-                        """.trimIndent(),
-                        globalRoles = listOf(GlobalRole.STELLIO_CREATOR)
-                    )
-                    subjectReferentialService.create(subjectReferential)
-                }
+            createSubject(
+                apiTestUserId1!!,
+                userSubject(subjectId = apiTestUserId1!!, groupMembership = apiTestGroupId1)
+            )
+
+            if (!apiTestUserId2.isNullOrEmpty()) {
+                createSubject(
+                    apiTestUserId2!!,
+                    userSubject(subjectId = apiTestUserId2!!)
+                )
+            }
+
+            if (!apiTestUserId3.isNullOrEmpty()) {
+                createSubject(
+                    apiTestUserId3!!,
+                    userSubject(subjectId = apiTestUserId3!!, globalRoles = listOf(GlobalRole.STELLIO_ADMIN))
+                )
+            }
+
+            if (!apiTestGroupId1.isNullOrEmpty()) {
+                createSubject(
+                    apiTestGroupId1!!,
+                    groupSubject(apiTestGroupId1!!)
+                )
+            }
+
+            if (!apiTestUserId2.isNullOrEmpty()) {
+                createSubject(
+                    apiTestGroupId2!!,
+                    groupSubject(apiTestGroupId2!!)
+                )
+            }
         }
     }
+
+    fun userSubject(
+        subjectId: String,
+        globalRoles: List<GlobalRole> = listOf(GlobalRole.STELLIO_CREATOR),
+        groupMembership: String? = null
+    ): SubjectReferential =
+        SubjectReferential(
+            subjectId = subjectId,
+            subjectType = SubjectType.USER,
+            subjectInfo = """
+                {"type":"Property","value":{"username":"api-tests-user@stellio.io"}}
+            """.trimIndent(),
+            globalRoles = globalRoles,
+            groupsMemberships =
+            if (!groupMembership.isNullOrEmpty())
+                listOf(groupMembership)
+            else null
+        )
+
+    fun groupSubject(subjectId: String): SubjectReferential =
+        SubjectReferential(
+            subjectId = subjectId,
+            subjectType = SubjectType.GROUP,
+            subjectInfo = """
+                {"type":"Property","value":{"name":"Group 1"}}
+            """.trimIndent()
+        )
+
+    suspend fun createSubject(subjectId: String, subjectReferential: SubjectReferential) =
+        subjectReferentialService.retrieve(subjectId)
+            .tapLeft {
+                subjectReferentialService.create(subjectReferential)
+            }
 }

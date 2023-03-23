84e49f5a18cf4e3c1357db8dcd684eea4c96cf6b
Benoit Orihuela <benoit.orihuela@egm.io>
2023-03-23 08:06:21 +0100

    fix(subscription): create notifiedAt timestamp as in core services

--- shared/src/main/kotlin/com/egm/stellio/shared/model/Notification.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/Notification.kt
@@ -3,7 +3,6 @@ package com.egm.stellio.shared.model
 import com.egm.stellio.shared.util.JsonLdUtils.NGSILD_NOTIFICATION_TERM
 import com.egm.stellio.shared.util.toUri
 import java.net.URI
-import java.time.Instant
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.util.UUID
@@ -12,6 +11,6 @@ data class Notification(
     val id: URI = "urn:ngsi-ld:Notification:${UUID.randomUUID()}".toUri(),
     val type: String = NGSILD_NOTIFICATION_TERM,
     val subscriptionId: URI,
-    val notifiedAt: ZonedDateTime = Instant.now().atZone(ZoneOffset.UTC),
+    val notifiedAt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
     val data: List<Map<String, Any>>
 )

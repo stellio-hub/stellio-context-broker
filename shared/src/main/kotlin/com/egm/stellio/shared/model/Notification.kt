963d96fdd8ba5de582411c4929b0c0769b9fa5eb
Benoit Orihuela <benoit.orihuela@egm.io>
2023-03-23 08:59:06 +0100

    feat: add custom datetime serializer to ensure NGSI-LD format in notifications dates

--- shared/src/main/kotlin/com/egm/stellio/shared/model/Notification.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/Notification.kt
@@ -1,7 +1,9 @@
 package com.egm.stellio.shared.model
 
+import com.egm.stellio.shared.util.DateTimeSerializer
 import com.egm.stellio.shared.util.JsonLdUtils.NGSILD_NOTIFICATION_TERM
 import com.egm.stellio.shared.util.toUri
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import java.net.URI
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
@@ -11,6 +13,7 @@ data class Notification(
     val id: URI = "urn:ngsi-ld:Notification:${UUID.randomUUID()}".toUri(),
     val type: String = NGSILD_NOTIFICATION_TERM,
     val subscriptionId: URI,
+    @JsonSerialize(using = DateTimeSerializer::class)
     val notifiedAt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
     val data: List<Map<String, Any>>
 )

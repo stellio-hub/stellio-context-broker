c387d8331b3e5c85166b00e7c7bb06ebbb6ee9a5
gpoujol <gael.poujol@egm.io>
2023-03-31 09:01:30 +0200

    fix: update instance id column if it is different also for no expanded payload

--- search-service/src/main/resources/db/migration/V0_33__fix_instance_id_difference_in_its_column_and_payload.sql
+++ search-service/src/main/resources/db/migration/V0_33__fix_instance_id_difference_in_its_column_and_payload.sql
@@ -1,3 +1,7 @@
 update attribute_instance
 set instance_id = trim('"' FROM jsonb_path_query_first(payload, '$."https://uri.etsi.org/ngsi-ld/instanceId"[0]."@id"')::text)
 where instance_id != trim('"' FROM jsonb_path_query_first(payload, '$."https://uri.etsi.org/ngsi-ld/instanceId"[0]."@id"')::text);
+
+update attribute_instance
+set instance_id = trim('"' FROM jsonb_path_query_first(payload, '$."instanceId"')::text)
+where instance_id != trim('"' FROM jsonb_path_query_first(payload, '$."instanceId"')::text);

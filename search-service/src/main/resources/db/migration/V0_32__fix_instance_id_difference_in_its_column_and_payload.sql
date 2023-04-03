ddec30e0ee5554a135ece4f13cefb726392fc38b
gpoujol <gael.poujol@egm.io>
2023-04-03 13:52:27 +0200

    chore: improve comment on migration script

--- search-service/src/main/resources/db/migration/V0_32__fix_instance_id_difference_in_its_column_and_payload.sql
+++ search-service/src/main/resources/db/migration/V0_32__fix_instance_id_difference_in_its_column_and_payload.sql
@@ -1,18 +1,16 @@
--- this sql query allows you to modify the instanceId of the column if it is different from the one contained in the payload
--- this sql query works for payloads in expand format
--- if the key of the json path does not exist it returns a null
--- and the postgres operator returns false if one of the two parameters is null (https://www.postgresql.org/docs/current/functions-comparison.html)
+-- we modify the instancesId of the column if it is different from the one is contained in the payload
+-- this sql query works for expanded payload
+-- if the result of jsonb_path_query_first is null the operation returns false (https://www.postgresql.org/docs/current/functions-comparison.html)
 update attribute_instance
 set instance_id = jsonb_path_query_first(payload, '$."https://uri.etsi.org/ngsi-ld/instanceId"[0]')->>'@id'
 where instance_id != jsonb_path_query_first(payload, '$."https://uri.etsi.org/ngsi-ld/instanceId"[0]')->>'@id';
 
--- this sql query is the same as above but for non expandable formats (version before v2)
+-- this sql query is the same as above but for not expanded payload (version before v2)
 update attribute_instance
 set instance_id = jsonb_path_query_first(payload, '$')->>'instanceId'
 where instance_id != jsonb_path_query_first(payload, '$')->>'instanceId';
 
--- the following sql queries allow to modify the instanceId of the audit table
--- this allows to no longer have similar instanceId between the two attributes instances tables
+-- Currently the two attributes instances tables have the same instanceId. We fix this to only have different instanceId
 update attribute_instance_audit
 set instance_id = concat('urn:ngsi-ld:Instance:', gen_random_uuid());
 

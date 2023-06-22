34e3df4c6ab9e71f8d2e94ae0e21287468b224c7
Benoit Orihuela <benoit.orihuela@egm.io>
2023-06-22 08:25:51 +0200

    fix: response content type and execution priority of tenant filter
    - set the highest precedence for the tenant filter (was previously overridden when auth is enabled)
    - add the application/json content type to the response when an error is raised from the filter

--- shared/src/main/kotlin/com/egm/stellio/shared/web/TenantWebFilter.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/web/TenantWebFilter.kt
@@ -9,9 +9,12 @@ import com.egm.stellio.shared.util.isURI
 import com.egm.stellio.shared.util.toUri
 import jakarta.annotation.PostConstruct
 import org.slf4j.LoggerFactory
+import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.core.io.buffer.DefaultDataBufferFactory
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
@@ -24,7 +27,7 @@ const val NGSILD_TENANT_HEADER = "NGSILD-Tenant"
 val DEFAULT_TENANT_URI = "urn:ngsi-ld:tenant:default".toUri()
 
 @Component
-@Order(0)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 class TenantWebFilter(
     private val applicationProperties: ApplicationProperties
 ) : WebFilter {
@@ -47,6 +50,7 @@ class TenantWebFilter(
         if (!tenantUri.isURI()) {
             logger.error("Requested tenant is not a valid URI: $tenantUri")
             exchange.response.setStatusCode(HttpStatus.BAD_REQUEST)
+            exchange.response.headers[CONTENT_TYPE] = MediaType.APPLICATION_JSON_VALUE
             val errorResponse = serializeObject(BadRequestDataResponse(invalidUriMessage("$tenantUri (tenant)")))
             return exchange.response.writeWith(
                 Flux.just(DefaultDataBufferFactory().wrap(errorResponse.toByteArray()))
@@ -54,6 +58,7 @@ class TenantWebFilter(
         } else if (!tenantsUris.contains(tenantUri.toUri())) {
             logger.error("Unknown tenant requested: $tenantUri")
             exchange.response.setStatusCode(HttpStatus.NOT_FOUND)
+            exchange.response.headers[CONTENT_TYPE] = MediaType.APPLICATION_JSON_VALUE
             val errorResponse = serializeObject(NonexistentTenantResponse("Tenant $tenantUri does not exist"))
             return exchange.response.writeWith(
                 Flux.just(DefaultDataBufferFactory().wrap(errorResponse.toByteArray()))

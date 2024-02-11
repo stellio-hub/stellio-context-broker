c56e11db2149731aaf30160fe0a052c50746f2ad
Benoit Orihuela <benoit.orihuela@egm.io>
2024-02-11 17:32:31 +0100

    align to Kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ subprojects {
     configurations.matching { it.name == "detekt" }.all {
         resolutionStrategy.eachDependency {
             if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("1.9.21")
+                useVersion("1.9.22")
             }
         }
     }

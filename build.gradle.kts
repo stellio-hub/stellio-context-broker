9cdb63cd2ab020b78357e38868c353761a43420f
Benoit Orihuela <benoit.orihuela@egm.io>
2024-10-19 16:52:42 +0200

    finalize detekt upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,7 +107,7 @@ subprojects {
     configurations.matching { it.name == "detekt" }.all {
         resolutionStrategy.eachDependency {
             if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("1.9.23")
+                useVersion("2.0.10")
             }
         }
     }

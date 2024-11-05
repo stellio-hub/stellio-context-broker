d867d399e6f3dd42f05ef5889ede48828d7836b9
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2024-11-05 15:12:24 +0100

    feat: spring 3.3.5 + configuration du firewall

---
+++ shared/src/main/kotlin/com/egm/stellio/shared/config/FirewallConfig.kt
@@ -0,0 +1,29 @@
+package com.egm.stellio.shared.config
+
+import org.springframework.beans.BeansException
+import org.springframework.beans.factory.config.BeanPostProcessor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.web.server.WebFilterChainProxy
+import org.springframework.security.web.server.firewall.StrictServerWebExchangeFirewall
+
+@Configuration
+class FirewallConfig {
+
+    @Bean
+    fun beanPostProcessor(): BeanPostProcessor {
+        return object : BeanPostProcessor {
+            @Throws(BeansException::class)
+            override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+                if (bean is WebFilterChainProxy) {
+                    val firewall = StrictServerWebExchangeFirewall()
+                    firewall.setAllowUrlEncodedSlash(true)
+                    firewall.setAllowUrlEncodedDoubleSlash(true)
+                    firewall.setAllowSemicolon(true)
+                    bean.setFirewall(firewall)
+                }
+                return bean
+            }
+        }
+    }
+}

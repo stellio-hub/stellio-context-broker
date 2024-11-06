00a88233195dc15d8e256a403c14981e1ee24122
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2024-11-06 14:41:23 +0100

    feat: config also for apigateway

--- api-gateway/src/main/kotlin/com/egm/stellio/apigateway/config/FirewallConfig.kt
+++ api-gateway/src/main/kotlin/com/egm/stellio/apigateway/config/FirewallConfig.kt
@@ -1,4 +1,4 @@
-package com.egm.stellio.shared.config
+package com.egm.stellio.apigateway.config
 
 import org.springframework.beans.BeansException
 import org.springframework.beans.factory.config.BeanPostProcessor

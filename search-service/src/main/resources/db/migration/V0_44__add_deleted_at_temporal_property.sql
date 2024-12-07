a85fbc972a78a4265aee33a13f662b61098b7300
Benoit Orihuela <benoit.orihuela@egm.io>
2024-12-07 12:40:44 +0100

    feat(core): handle previous deleted attributes when creating a new one

--- search-service/src/main/resources/db/migration/V0_44__add_deleted_at_temporal_property.sql
+++ search-service/src/main/resources/db/migration/V0_44__add_deleted_at_temporal_property.sql
@@ -1,5 +1,13 @@
 ALTER TABLE temporal_entity_attribute
-ADD COLUMN deleted_at timestamp with time zone;
+    ADD COLUMN deleted_at timestamp with time zone;
 
 ALTER TABLE entity_payload
-ADD COLUMN deleted_at timestamp with time zone;
+    ADD COLUMN deleted_at timestamp with time zone;
+
+DROP INDEX IF EXISTS temporal_entity_attribute_null_datasetid_uniqueness;
+
+ALTER TABLE temporal_entity_attribute
+    DROP CONSTRAINT temporal_entity_attribute_uniqueness;
+
+ALTER TABLE temporal_entity_attribute
+    ADD CONSTRAINT temporal_entity_attribute_uniqueness UNIQUE NULLS NOT DISTINCT (entity_id, attribute_name, dataset_id);

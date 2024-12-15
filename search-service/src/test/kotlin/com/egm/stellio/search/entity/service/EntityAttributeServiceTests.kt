18115e79ce01f44aa03405dcbb48187bb407d538
Benoit Orihuela <benoit.orihuela@egm.io>
2024-12-15 18:17:05 +0100

    feat(core): add support for NGSI-LD Null in Partial Attribute Update operation

--- search-service/src/test/kotlin/com/egm/stellio/search/entity/service/EntityAttributeServiceTests.kt
+++ search-service/src/test/kotlin/com/egm/stellio/search/entity/service/EntityAttributeServiceTests.kt
@@ -571,6 +571,50 @@ class EntityAttributeServiceTests : WithTimescaleContainer, WithKafkaContainer {
     }
 
     @Test
+    fun `it should delete an attribute in partial attribute update operation if its value is NGSI-LD null`() = runTest {
+        val rawEntity = loadSampleData()
+
+        coEvery { attributeInstanceService.create(any()) } returns Unit.right()
+        coEvery {
+            attributeInstanceService.addDeletedAttributeInstance(any(), any(), any(), any())
+        } returns Unit.right()
+
+        entityAttributeService.createAttributes(rawEntity, APIC_COMPOUND_CONTEXTS).shouldSucceed()
+
+        val createdAt = ngsiLdDateTime()
+        val propertyToDelete = loadSampleData("fragments/beehive_mergeAttribute_null_fragment.json")
+        val expandedAttribute = expandAttribute(INCOMING_PROPERTY, propertyToDelete, APIC_COMPOUND_CONTEXTS)
+        entityAttributeService.partialUpdateAttribute(
+            beehiveTestCId,
+            expandedAttribute,
+            createdAt,
+            null
+        ).shouldSucceedWith { updateResult ->
+            val updatedDetails = updateResult.updated
+            assertEquals(1, updatedDetails.size)
+            assertEquals(1, updatedDetails.filter { it.updateOperationResult == UpdateOperationResult.DELETED }.size)
+        }
+
+        coVerify(exactly = 1) {
+            attributeInstanceService.addDeletedAttributeInstance(
+                any(),
+                NGSILD_NULL,
+                createdAt,
+                expandAttribute(
+                    INCOMING_PROPERTY,
+                    """
+                        {
+                            "type": "Property",
+                            "value": "urn:ngsi-ld:null"
+                        }
+                    """.trimIndent(),
+                    NGSILD_TEST_CORE_CONTEXTS
+                ).second[0]
+            )
+        }
+    }
+
+    @Test
     fun `it should replace an entity attribute`() = runTest {
         val rawEntity = loadSampleData()
 

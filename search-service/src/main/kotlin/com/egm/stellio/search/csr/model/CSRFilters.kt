967a3a851210e9503ce18b424d56726ebb7320eb
Thomas Bousselin <61795238+thomasBousselin@users.noreply.github.com>
2025-02-13 16:52:58 +0100

    feature: add filter to query csr (+ query csr tests) (#1337)
    * fix: add filter to query csr (+ query csr tests)
    
    * fix: detekt
    
    * fix: PR comments

--- search-service/src/main/kotlin/com/egm/stellio/search/csr/model/CSRFilters.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/csr/model/CSRFilters.kt
@@ -1,6 +1,14 @@
 package com.egm.stellio.search.csr.model
 
+import arrow.core.Either
+import arrow.core.raise.either
+import com.egm.stellio.shared.model.APIException
 import com.egm.stellio.shared.model.EntityTypeSelection
+import com.egm.stellio.shared.queryparameter.QueryParameter
+import com.egm.stellio.shared.util.expandTypeSelection
+import com.egm.stellio.shared.util.toListOfUri
+import com.egm.stellio.shared.util.validateIdPattern
+import org.springframework.util.MultiValueMap
 import java.net.URI
 
 open class CSRFilters( // we should use a combination of EntitiesQuery TemporalQuery (when we implement all operations)
@@ -16,9 +24,9 @@ open class CSRFilters( // we should use a combination of EntitiesQuery TemporalQ
         operations: List<Operation>?
     ) :
         this(
-            ids = ids,
-            typeSelection = typeSelection,
-            idPattern = idPattern,
+            ids,
+            typeSelection,
+            idPattern,
             csf = operations?.joinToString("|") { "${ContextSourceRegistration::operations.name}==${it.key}" }
         )
 
@@ -28,9 +36,22 @@ open class CSRFilters( // we should use a combination of EntitiesQuery TemporalQ
         idPattern: String? = null,
         operations: List<Operation>? = null
     ) : this(
-        ids = ids,
-        typeSelection = types.joinToString("|"),
-        idPattern = idPattern,
+        ids,
+        types.joinToString("|"),
+        idPattern,
         operations = operations
     )
+
+    companion object {
+        fun fromQueryParameters(
+            queryParams: MultiValueMap<String, String>,
+            contexts: List<String>
+        ): Either<APIException, CSRFilters> = either {
+            val ids = queryParams.getFirst(QueryParameter.ID.key)?.split(",").orEmpty().toListOfUri().toSet()
+            val typeSelection = expandTypeSelection(queryParams.getFirst(QueryParameter.TYPE.key), contexts)
+            val idPattern = validateIdPattern(queryParams.getFirst(QueryParameter.ID_PATTERN.key)).bind()
+
+            CSRFilters(ids, typeSelection, idPattern)
+        }
+    }
 }

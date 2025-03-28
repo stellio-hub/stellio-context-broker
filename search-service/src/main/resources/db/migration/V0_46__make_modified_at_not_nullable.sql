17c2461ceda0809c2729e9ad27bb3dd1b43802c8
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2025-03-28 15:08:55 +0100

    fix: search service migration script name (+ remove non null for temporal instance)

--- search-service/src/main/resources/db/migration/V0_46__make_modified_at_not_nullable.sql
+++ search-service/src/main/resources/db/migration/V0_46__make_modified_at_not_nullable.sql
@@ -2,13 +2,6 @@ update entity_payload
 set modified_at = created_at
 where modified_at is null;
 
-alter table entity_payload
-    alter column modified_at set not null;
-
 update context_source_registration
 set modified_at = created_at
 where modified_at is null;
-
-alter table context_source_registration
-    alter column modified_at set not null;
-
--- subscription-service/src/main/resources/db/migration/V0_31__set_null_modified_at_to_created_at.sql
+++ subscription-service/src/main/resources/db/migration/V0_31__set_null_modified_at_to_created_at.sql
@@ -1,7 +1,3 @@
 update subscription
 set modified_at = created_at
 where modified_at is null;
-
-alter table subscription
-    alter column modified_at set not null;
-

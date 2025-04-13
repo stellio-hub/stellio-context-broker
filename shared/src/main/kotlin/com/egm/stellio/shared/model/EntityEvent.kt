b3e95f7e4235bc2897e181b561e8f550d395c6e2
Benoit Orihuela <benoit.orihuela@egm.io>
2025-04-13 15:38:20 +0200

    fixup: some previously removed unused code still in PR

--- shared/src/main/kotlin/com/egm/stellio/shared/model/EntityEvent.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/EntityEvent.kt
@@ -127,3 +127,5 @@ enum class EventsType {
     ATTRIBUTE_UPDATE,
     ATTRIBUTE_DELETE
 }
+
+fun unhandledOperationType(operationType: EventsType): String = "Entity event $operationType not handled."

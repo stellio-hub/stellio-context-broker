485be9a89175871e9481e825d2c543ff68167edd
Benoit Orihuela <benoit.orihuela@egm.io>
2025-05-10 09:31:35 +0200

    add tests for retrieval of linked entities

--- subscription-service/src/test/kotlin/com/egm/stellio/subscription/service/CoreAPIServiceTests.kt
+++ subscription-service/src/test/kotlin/com/egm/stellio/subscription/service/CoreAPIServiceTests.kt
@@ -1,13 +1,26 @@
 package com.egm.stellio.subscription.service
 
+import com.egm.stellio.shared.queryparameter.QueryParameter
 import com.egm.stellio.shared.util.APIC_HEADER_LINK
+import com.egm.stellio.shared.util.INCOMING_COMPACT_PROPERTY
 import com.egm.stellio.shared.util.loadSampleData
+import com.egm.stellio.shared.util.toUri
 import com.egm.stellio.shared.web.DEFAULT_TENANT_NAME
+import com.egm.stellio.shared.web.NGSILD_TENANT_HEADER
 import com.egm.stellio.subscription.config.WebClientConfig
+import com.egm.stellio.subscription.model.Endpoint
+import com.egm.stellio.subscription.model.Endpoint.AcceptType
+import com.egm.stellio.subscription.model.NotificationParams
+import com.egm.stellio.subscription.model.NotificationParams.FormatType
+import com.egm.stellio.subscription.model.NotificationParams.JoinType
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.okJson
 import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.verify
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -15,6 +28,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpHeaders
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
@@ -27,6 +41,8 @@ class CoreAPIServiceTests {
 
     @Autowired
     private lateinit var coreApiService: CoreAPIService
+
+    private val beehiveTestCId = "urn:ngsi-ld:BeeHive:TESTC".toUri()
 
     @Test
     fun `it should return one entity if the query sent to entity service matches one entity`() {
@@ -78,5 +94,52 @@ class CoreAPIServiceTests {
             assertEquals("urn:ngsi-ld:BeeHive:TESTC", compactedEntities[0]["id"])
             assertEquals("urn:ngsi-ld:BeeHive:TESTD", compactedEntities[1]["id"])
         }
+    }
+
+    @Test
+    fun `it should call the core API using the notification parameters from the subscription`() {
+        stubFor(
+            get(urlPathEqualTo("/ngsi-ld/v1/entities/$beehiveTestCId"))
+                .willReturn(
+                    okJson(
+                        listOf(
+                            loadSampleData("beehive.jsonld"),
+                            loadSampleData("beehive2.jsonld")
+                        ).toString()
+                    )
+                )
+        )
+
+        runBlocking {
+            val compactedEntities = coreApiService.retrieveLinkedEntities(
+                DEFAULT_TENANT_NAME,
+                beehiveTestCId,
+                NotificationParams(
+                    attributes = listOf(INCOMING_COMPACT_PROPERTY),
+                    format = FormatType.NORMALIZED,
+                    endpoint = Endpoint(
+                        uri = "http://localhost:8089/notification".toUri(),
+                        accept = AcceptType.JSONLD
+                    ),
+                    join = JoinType.FLAT,
+                    joinLevel = 1
+                ),
+                APIC_HEADER_LINK
+            )
+            assertEquals(2, compactedEntities.size)
+            assertEquals("urn:ngsi-ld:BeeHive:TESTC", compactedEntities[0]["id"])
+            assertEquals("urn:ngsi-ld:BeeHive:TESTD", compactedEntities[1]["id"])
+        }
+
+        verify(
+            getRequestedFor(urlPathEqualTo("/ngsi-ld/v1/entities/$beehiveTestCId"))
+                .withHeader(HttpHeaders.LINK, equalTo(APIC_HEADER_LINK))
+                .withHeader(HttpHeaders.ACCEPT, equalTo(AcceptType.JSONLD.accept))
+                .withHeader(NGSILD_TENANT_HEADER, equalTo(DEFAULT_TENANT_NAME))
+                .withQueryParam(QueryParameter.JOIN.key, equalTo(JoinType.FLAT.join))
+                .withQueryParam(QueryParameter.JOIN_LEVEL.key, equalTo("1"))
+                .withQueryParam(QueryParameter.OPTIONS.key, equalTo(FormatType.NORMALIZED.format))
+                .withQueryParam(QueryParameter.ATTRS.key, equalTo(INCOMING_COMPACT_PROPERTY))
+        )
     }
 }

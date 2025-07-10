a6c6e1a0449e9ad275133ad03bc2226886813f49
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2025-07-10 12:10:26 +0200

    feat: filter permission by target entity type and separate endpoint to get assigned permissions

--- search-service/src/main/kotlin/com/egm/stellio/search/authorization/permission/model/PermissionFilters.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/authorization/permission/model/PermissionFilters.kt
@@ -5,6 +5,7 @@ import arrow.core.raise.either
 import com.egm.stellio.shared.model.APIException
 import com.egm.stellio.shared.queryparameter.QueryParameter
 import com.egm.stellio.shared.util.Sub
+import com.egm.stellio.shared.util.expandTypeSelection
 import com.egm.stellio.shared.util.toListOfUri
 import org.springframework.util.MultiValueMap
 import java.net.URI
@@ -13,21 +14,30 @@ open class PermissionFilters(
     val ids: Set<URI>? = null,
     val action: Action? = null,
     val assignee: Sub? = null,
-    val assigner: Sub? = null
+    val assigner: Sub? = null,
+    val targetTypeSelection: String? = null,
+    val onlyGetPermission: OnlyGetPermission? = OnlyGetPermission.ADMIN,
 ) {
 
     companion object {
+        enum class OnlyGetPermission(val value: String) {
+            ADMIN("admin"),
+            ASSIGNED("assigned")
+        }
+
         fun fromQueryParameters(
             queryParams: MultiValueMap<String, String>,
-            contexts: List<String>
+            contexts: List<String>,
+            onlyGetPermission: OnlyGetPermission? = null
         ): Either<APIException, PermissionFilters> = either {
             val ids = queryParams.getFirst(QueryParameter.ID.key)?.split(",").orEmpty().toListOfUri().toSet()
 
             val action = queryParams.getFirst(QueryParameter.ACTION.key)?.let { Action.fromString(it).bind() }
             val assignee = queryParams.getFirst(QueryParameter.ASSIGNEE.key)
             val assigner = queryParams.getFirst(QueryParameter.ASSIGNER.key)
+            val targetType = expandTypeSelection(queryParams.getFirst(QueryParameter.TYPE.key), contexts)
 
-            PermissionFilters(ids, action, assignee, assigner)
+            PermissionFilters(ids, action, assignee, assigner, targetType, onlyGetPermission)
         }
     }
 }

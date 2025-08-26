e00d8aea32b4a1c3dca6e4437338908c59cf8df1
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2025-08-26 12:01:54 +0200

    fix: fix migration when duplicated global permission

--- search-service/src/main/resources/db/migration/V0_48__uniqueness_for_null_assignee.sql
+++ search-service/src/main/resources/db/migration/V0_48__uniqueness_for_null_assignee.sql
@@ -1,6 +1,19 @@
 alter table permission
     drop constraint entity_access_rights_uniqueness;
 
+WITH duplicates AS (
+    SELECT id,
+           ROW_NUMBER() OVER (
+               PARTITION BY assignee, action, target_id
+               ORDER BY id
+               ) AS rn
+    FROM permission
+)
+DELETE FROM permission
+WHERE id IN (
+    SELECT id FROM duplicates WHERE rn > 1
+);
+
 alter table permission
     add constraint permission_uniqueness
         unique NULLS NOT DISTINCT (assignee, action, target_id);

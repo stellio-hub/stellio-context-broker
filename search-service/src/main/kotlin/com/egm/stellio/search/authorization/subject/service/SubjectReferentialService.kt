40886b0c8e5a568d9149d5b83990755c6cadf2fe
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-02-09 17:47:44 +0100

    fix: test

--- search-service/src/main/kotlin/com/egm/stellio/search/authorization/subject/service/SubjectReferentialService.kt
+++ search-service/src/main/kotlin/com/egm/stellio/search/authorization/subject/service/SubjectReferentialService.kt
@@ -34,7 +34,6 @@ import org.springframework.r2dbc.core.bind
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-
 @Service
 class SubjectReferentialService(
     private val applicationProperties: ApplicationProperties,

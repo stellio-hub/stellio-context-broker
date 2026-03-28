dfec63c94c2ba73079cfdfb2ac560c9426727a16
Benoit Orihuela <benoit.orihuela@egm.io>
2026-03-28 17:55:30 +0100

    Revert "feat(ci): migrate from Jib to Cloud Native Buildpack (#1579)"
    This reverts commit 0cba7028c63aec733cf49acf111aeca209680d59.
    
    # Conflicts:
    #	api-gateway/build.gradle.kts
    #	build.gradle.kts
    #	search-service/build.gradle.kts
    #	subscription-service/build.gradle.kts

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -1,4 +1,7 @@
+import com.google.cloud.tools.jib.gradle.PlatformParameters
+
 plugins {
+    id("com.google.cloud.tools.jib")
     id("org.springframework.boot")
 }
 
@@ -20,14 +23,9 @@ springBoot {
     }
 }
 
-tasks.bootBuildImage {
-    imageName = "stellio/stellio-api-gateway:${project.version}"
-    imagePlatform = "linux/amd64"
-
-    val buildpackEnvironment = project.ext["buildpackEnvironment"] as? Map<String, String> ?: emptyMap()
-    val buildpackRuntimeEnvironment = project.ext["buildpackRuntimeEnvironment"] as? Map<String, String> ?: emptyMap()
-    val buildpackOciLabels = project.ext["buildpackOciLabels"] as? Map<String, String> ?: emptyMap()
-    environment = buildpackEnvironment
-        .plus(buildpackRuntimeEnvironment)
-        .plus(buildpackOciLabels)
-}
+jib.from.image = project.ext["jibFromImage"].toString()
+jib.from.platforms.addAll(project.ext["jibFromPlatforms"] as List<PlatformParameters>)
+jib.to.image = "stellio/stellio-api-gateway:${project.version}"
+jib.container.ports = listOf("8080")
+jib.container.creationTime.set(project.ext["jibContainerCreationTime"].toString())
+jib.container.labels.putAll(project.ext["jibContainerLabels"] as Map<String, String>)

ac922fa8b4fc5048fc28aac9bbefdd18a7ee057b
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-04-07 11:00:45 +0200

    feat: test all temporal value search

--- search-service/src/test/kotlin/com/egm/stellio/search/common/DBConversionUtilsTests.kt
+++ search-service/src/test/kotlin/com/egm/stellio/search/common/DBConversionUtilsTests.kt
@@ -4,6 +4,7 @@ import com.egm.stellio.search.common.util.toJson
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
+import java.net.URI
 import kotlin.test.assertEquals
 
 @ActiveProfiles("test")
@@ -27,5 +28,23 @@ class DBConversionUtilsTests {
     fun `toJson should work for a double`() = runTest {
         val jsonBoolean = 12.0.toJson()
         assertEquals(jsonBoolean.asString(), "12.0")
+    }
+
+    @Test
+    fun `toJson should work for a list`() = runTest {
+        val jsonList = listOf(12.0, 13).toJson()
+        assertEquals(jsonList.asString(), "[12.0,13]")
+    }
+
+    @Test
+    fun `toJson should work for a map`() = runTest {
+        val jsonList = mapOf("property1" to 1, "property2" to 2).toJson()
+        assertEquals(jsonList.asString(), """{"property1":1,"property2":2}""")
+    }
+
+    @Test
+    fun `toJson should work for a URI`() = runTest {
+        val jsonList = URI("urn:ngsi-ld:1").toJson()
+        assertEquals("\"urn:ngsi-ld:1\"", jsonList.asString())
     }
 }

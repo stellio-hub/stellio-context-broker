feeea27cfd14c556b0a219942042ba0fe850295a
Benoit Orihuela <benoit.orihuela@egm.io>
2026-04-23 21:22:04 +0200

    chore: remove unused GraalVM dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ plugins {
     // and the shared lib is obviously not one
     id("org.springframework.boot") version "4.0.6" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
-    id("org.graalvm.buildtools.native") version "1.0.0"
     kotlin("jvm") version "2.3.10" apply false
     kotlin("plugin.spring") version "2.3.10" apply false
     id("com.google.cloud.tools.jib") version "3.5.3" apply false

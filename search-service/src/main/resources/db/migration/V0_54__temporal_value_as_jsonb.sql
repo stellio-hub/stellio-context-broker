87ea99254dc5e1ff3d972b4e0e7f3f961af3a0c6
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-06-17 10:43:24 +0200

    fix: split migration for compressed table

--- search-service/src/main/resources/db/migration/V0_54__temporal_value_as_jsonb.sql
+++ search-service/src/main/resources/db/migration/V0_54__temporal_value_as_jsonb.sql
@@ -1,10 +1,30 @@
+-- change value type to jsonb in multiple steps to support compressed table
+
+-- attribute_instance
 ALTER TABLE attribute_instance
-    ALTER COLUMN value TYPE JSONB
-        USING to_jsonb(value);
+    ADD COLUMN json_value JSONB;
+
+UPDATE attribute_instance
+SET json_value = to_jsonb(value);
+
+ALTER TABLE attribute_instance
+    DROP COLUMN value;
+
+ALTER TABLE attribute_instance
+    RENAME COLUMN json_value TO value;
+
+-- attribute_instance_audit
+ALTER TABLE attribute_instance_audit
+    ADD COLUMN json_value JSONB;
+
+UPDATE attribute_instance_audit
+SET json_value = to_jsonb(value);
 
 ALTER TABLE attribute_instance_audit
-    ALTER COLUMN value TYPE JSONB
-        USING to_jsonb(value);
+    DROP COLUMN value;
+
+ALTER TABLE attribute_instance_audit
+    RENAME COLUMN json_value TO value;
 
 
 -- field who already had a json value

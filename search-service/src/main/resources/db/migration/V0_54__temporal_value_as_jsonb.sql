015cf7097d5167d50b94ece24c478ca0a6653257
Thomas Bousselin <61795238+thomasBousselin@users.noreply.github.com>
2026-06-17 14:50:18 +0200

    fix: split migration for compressed table (#1779)

--- search-service/src/main/resources/db/migration/V0_54__temporal_value_as_jsonb.sql
+++ search-service/src/main/resources/db/migration/V0_54__temporal_value_as_jsonb.sql
@@ -1,16 +1,42 @@
+-- change value type to jsonb in multiple steps to support compressed table
+
+-- attribute_instance
 ALTER TABLE attribute_instance
-    ALTER COLUMN value TYPE JSONB
-        USING to_jsonb(value);
+    ADD COLUMN json_value JSONB;
+
+UPDATE attribute_instance
+SET json_value = to_jsonb(value)
+WHERE value IS NOT NULL;
+
+
+ALTER TABLE attribute_instance
+    DROP COLUMN value;
+
+ALTER TABLE attribute_instance
+    RENAME COLUMN json_value TO value;
+
+-- attribute_instance_audit
+ALTER TABLE attribute_instance_audit
+    ADD COLUMN json_value JSONB;
+
+
+UPDATE attribute_instance_audit
+SET json_value = to_jsonb(value)
+WHERE value IS NOT NULL;
 
 ALTER TABLE attribute_instance_audit
-    ALTER COLUMN value TYPE JSONB
-        USING to_jsonb(value);
+    DROP COLUMN value;
+
+ALTER TABLE attribute_instance_audit
+    RENAME COLUMN json_value TO value;
 
 
 -- field who already had a json value
 UPDATE attribute_instance
 SET value = (value #>> '{}')::jsonb
-WHERE payload #>> '{@type, 0}' in (
+WHERE
+    value IS NOT NULL
+    AND payload #>> '{@type, 0}' in (
     'https://uri.etsi.org/ngsi-ld/LanguageProperty',
     'https://uri.etsi.org/ngsi-ld/JsonProperty',
     'https://uri.etsi.org/ngsi-ld/VocabProperty'
@@ -19,7 +45,9 @@ AND LEFT(value #>> '{}', 1) in ('{', '['); -- avoid transforming 'urn:ngsi-ld:nu
 
 UPDATE attribute_instance_audit
 SET value = (value #>> '{}')::jsonb
-WHERE payload #>> '{@type, 0}' in (
+WHERE
+    value IS NOT NULL
+    AND payload #>> '{@type, 0}' in (
     'https://uri.etsi.org/ngsi-ld/LanguageProperty',
     'https://uri.etsi.org/ngsi-ld/JsonProperty',
     'https://uri.etsi.org/ngsi-ld/VocabProperty'

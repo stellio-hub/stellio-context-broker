e6b5d6c5bbba7ee95b07cc3220c1d77223ee07aa
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-07-30 17:11:50 +0200

    fix: JcksonException returning InternalError

--- shared/src/main/kotlin/com/egm/stellio/shared/model/ApiExceptions.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/ApiExceptions.kt
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import tools.jackson.core.JacksonException
 import java.net.URI
 
 const val TYPE_PROPERTY = "type"
@@ -259,6 +260,7 @@ fun Throwable.toAPIException(specificMessage: String? = null): APIException =
                     "Caused by: $this"
                 )
             else BadRequestDataException(UNEXPECTED_ERROR_PARSING_PAYLOAD_MESSAGE, "Caused by: $this")
+        is JacksonException -> BadRequestDataException(specificMessage ?: this.localizedMessage)
         else -> InternalErrorException(specificMessage ?: this.localizedMessage)
     }
 

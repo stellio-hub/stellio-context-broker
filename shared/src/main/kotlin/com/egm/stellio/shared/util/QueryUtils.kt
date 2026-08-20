b9f6810a639da6b29d5189101932b30652d2d02c
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-08-20 13:53:56 +0200

    feat: add test for singlequote

--- shared/src/main/kotlin/com/egm/stellio/shared/util/QueryUtils.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/util/QueryUtils.kt
@@ -17,7 +17,7 @@ fun Iterable<String>.toTypeSelection() = this.joinToString(",")
 // Work for String and URI
 // can't do two functions with the same name since Uri are String in the jvm
 fun Iterable<Any>.toSqlList(): String =
-    "(${this.joinToString(",") { "'${it.toString().escapeSingleQuotes()}'" }} )"
+    "(${this.joinToString(",") { "'${it.toString().escapeSingleQuotes()}'" }})"
 
 fun Iterable<Any?>.toSqlArray(): String =
     "ARRAY[${this.joinToString(",") { "'${it.toString().escapeSingleQuotes()}'" }}]"

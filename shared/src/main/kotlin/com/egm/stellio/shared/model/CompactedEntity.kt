af8fb2d0b742592e30839c2f19bb4108c2629ecc
Thomas BOUSSELIN <thomas.bousselin@egm.io>
2026-09-16 16:46:36 +0200

    feat: concise listRelationships

--- shared/src/main/kotlin/com/egm/stellio/shared/model/CompactedEntity.kt
+++ shared/src/main/kotlin/com/egm/stellio/shared/model/CompactedEntity.kt
@@ -12,6 +12,7 @@ import com.egm.stellio.shared.model.AttributeCompactedType.LISTRELATIONSHIP
 import com.egm.stellio.shared.model.AttributeCompactedType.PROPERTY
 import com.egm.stellio.shared.model.AttributeCompactedType.RELATIONSHIP
 import com.egm.stellio.shared.model.AttributeCompactedType.VOCABPROPERTY
+import com.egm.stellio.shared.model.NGSILD_ENTITY_LIST_TERM
 import com.egm.stellio.shared.queryparameter.QueryParameter
 import com.egm.stellio.shared.util.ErrorMessages.QueryParameter.NO_ENTITY_MEMBER_AFTER_PROJECTION_MESSAGE
 import com.egm.stellio.shared.util.FEATURES_PROPERTY_TERM
@@ -184,15 +185,25 @@ fun CompactedEntity.toConciseAttributes(): Map<String, Any> =
 private fun conciseAttribute(value: Map<String, Any>, isMultiInstanceAttribute: Boolean = false): Any =
     value.minus(NGSILD_TYPE_TERM)
         .mapValues { (key, value) ->
-            if (!JSONLD_COMPACTED_ATTRIBUTE_CORE_MEMBERS.contains(key))
-                conciseAttribute(value as Map<String, Any>)
-            else value
+            when {
+                key == NGSILD_ENTITY_LIST_TERM && value is List<*> ->
+                    value.map { (it as CompactedEntity).toConciseAttributes() }
+                !JSONLD_COMPACTED_ATTRIBUTE_CORE_MEMBERS.contains(key) ->
+                    conciseAttribute(value as Map<String, Any>)
+                else -> value
+            }
         }.let {
             when (value[NGSILD_TYPE_TERM] as String) {
                 PROPERTY.key, GEOPROPERTY.key ->
                     if (it.keys == setOf(NGSILD_VALUE_TERM) && !isMultiInstanceAttribute)
                         it[NGSILD_VALUE_TERM]!!
                     else it
+                LISTRELATIONSHIP.key ->
+                    it.mapValues { (key, value) ->
+                        if (key == NGSILD_LISTRELATIONSHIP_OBJECT_LIST_TERM && value is List<*>)
+                            value.map { (it as? Map<*, *>)?.get(NGSILD_OBJECT_TERM) ?: it }
+                        else value
+                    }
                 else -> it
             }
         }
